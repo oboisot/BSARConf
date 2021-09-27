ea0f03acd3ea127e9b70aa268dfa4c93112ccc2d
oboisot <olivier.boisot@gmail.com>
2021-09-27 17:57:20 +0200

    adding constants file

---
+++ js/scripts/constants.js
@@ -0,0 +1,11 @@
+/* Mathematical constants */
+export const PI         = Math.PI;
+export const HALF_PI    = 0.5 * Math.PI;
+export const DOUBLE_PI  = 2.0 * Math.PI;
+export const DEG_TO_RAD = Math.PI / 180.0;
+export const RAD_TO_DEG = 180.0 / Math.PI;
+/* Physical constants */
+export const C0              = 299792458.0;        // speed of light in vacuum [m/s]
+export const K               = 1.380649e-23;       // Boltzmann constant [J/K]
+export const RES_FACTOR      = 0.8858929413789047; // twice the solution of sinc^2(x) = 0.5 for -3.01dB resolutions
+export const RES_AREA_FACTOR = 0.7848063035849675; // RES_FACTOR * RES_FACTOR
